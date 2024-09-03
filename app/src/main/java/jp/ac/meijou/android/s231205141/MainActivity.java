ece5533554657ee02aa46826f0f16be28206dbb0
Tomomaru <136682030+tomomaruit@users.noreply.github.com>
2024-09-03 12:18:37 +0900

    Day2-AM:TextViewとImageViewの演習

--- app/src/main/java/jp/ac/meijou/android/s231205141/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205141/MainActivity.java
@@ -1,24 +1,38 @@
 package jp.ac.meijou.android.s231205141;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s231205141.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //TextView text = findViewById(R.id.text);
+        binding.text.setText(R.string.text);
+        var bubble = ContextCompat.getDrawable(this, R.drawable.baseline_chat_bubble);
+        binding.imageView2.setImageDrawable(bubble);
     }
 }
